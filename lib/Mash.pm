3fa236e4d481a1ca61c56fb2820ae7f66bba6fc4
Lee Katz - Aspen <gzu2@cdc.gov>
2019-03-04 11:29:30 -0500

    v0.1

--- lib/Mash.pm
+++ lib/Mash.pm
@@ -6,18 +6,12 @@ use Exporter qw(import);
 use File::Basename qw/fileparse basename dirname/;
 use Data::Dumper;
 
-use JSON qw/from_json/;
-use Bio::Seq;
-use Bio::Tree::Tree;
-use Bio::Matrix::Generic;
-use Bio::SimpleAlign;
-use Bio::Align::DNAStatistics;
-use Bio::Tree::DistanceFactory;
+use JSON ();
+use Encode qw/encode decode/;
 
 our $VERSION = 0.1;
 
-our @EXPORT_OK = qw(
-         );
+our @EXPORT_OK = qw(raw_mash_distance);
 
 local $0=basename $0;
 
@@ -44,12 +38,9 @@ A module to read `mash info` output and transform it
   system("mash sketch *.fastq.gz > all.msh");
   die if $?;
   # Read the mash file.
-  my $msh = Mash->new(["all.msh"]);
-  # Get a Bio::Tree::Tree object
-  my $tree = $msh->tree;
-
-  # Do something with the tree, e.g.,
-  print $tree->as_text("newick");
+  my $msh = Mash->new("all.msh");
+  # All-vs-all distances
+  my $distHash = $msh->dist($msh);
 
 =head1 DESCRIPTION
 
@@ -63,13 +54,7 @@ This is a module to read mash files produced by the Mash executable. For more in
 
 Create a new instance of Mash.  One object per set of files.
 
-  Arguments:  List of filenames
-              Hash of options (none so far)
-  Returns:    Mash object
-
-  -or-
-
-  Arguments:  Hash of data
+  Arguments:  Sketch filename
               Hash of options (none so far)
   Returns:    Mash object
 
@@ -78,76 +63,29 @@ Create a new instance of Mash.  One object per set of files.
 =cut
 
 sub new{
-  my($class,$mshData,$settings)=@_;
+  my($class,$filename,$settings)=@_;
 
   my $self={
-    file      => [],
-    info      => {},
-    names     => [],
-    hashes    => {},
-    distance  => {},
+    file      => $filename,
+    kmer      => -1, # eg, 21
+    alphabet  => "", # eg, AGCT
+    canonical => -1, # eg, true/false
+    sketchSize=> -1, # eg, 1000
+    hashType  => "", # eg, "MurmurHash3_x64_128"
+    hashBits  => -1, # eg, 64
+    hashSeed  => -1, # eg, 42
+    sketches  => [], # Array of hashes. Each hash has keys
+                     #  name    => original filename
+                     #  length  => integer of estimated genome size
+                     #  comment => string description
+                     #  hashes  => list of integers
   };
   bless($self,$class);
-
-  # How should we initialize?
-  if(ref($mshData) eq 'HASH'){
-    # Make sure every key is present that is needed
-    my @requiredKeys=qw(info names hashes);
-    for my $key(@requiredKeys){
-      if(!$$mshData{$key}){
-        die "ERROR: could not find key '$key' in hash for $class";
-      }
-      if(ref($$mshData{$key}) ne ref($$self{$key})){
-        die "ERROR: data type for '$key' does not match ".ref($$self{$key});
-      }
-    }
-    $self->_newByHash($mshData,$settings);
-  } elsif(ref($mshData) eq 'ARRAY'){
-    $self->_newByMashFile($mshData,$settings);
-  } else {
-    die "ERROR: the first parameter must be a list of mash file(s)";
-  }
-  return $self;
-}
-
-
-# Initialize with a mash file
-sub _newByMashFile{
-  my($self,$file,$settings)=@_;
-
-  $self->{file}=$file;
-  if(ref($file) ne 'ARRAY'){
-    die "ERROR: the first parameter must be a list of mash file(s)";
-  }
-
-  # Gather info from each file. $self->{info} and
-  # $self->{hashes} gets updated.
-  for my $f(@$file){
-    $self->addMashFile($f);
-  }
-
-  # Test that all metadata for mash are the same as the
-  # first genome in the set.  I.e., they are all the same.
-  my $info=$self->{info};
-  my @genomeName=keys(%$info);
-  my $ref=shift(@genomeName); 
-  for my $g(@genomeName){
-    # TODO don't worry about sketch size b/c you can take
-    # first X sketches from each where X is the smallest
-    # number of sketches in the set. For now though to make
-    # things simple, just take exact numbers.
-    for my $key(qw(kmer alphabet preserveCase canonical sketchSize hashType hashBits hashSeed)){
-      if($$info{$g}{$key} ne $$info{$ref}{$key}){
-        die "ERROR: genomes $ref and $g are incompatible under property $key";
-      }
-    }
-  }
-
-  # Set a sorted list of names
-  $self->{names}=[sort {$a cmp $b} keys(%{ $self->{info} })];
+  $self->file($filename);
 
   return $self;
 }
+
 
 =pod
 
@@ -158,114 +96,182 @@ sub _newByMashFile{
 Returns a hash ref that describes a single mash file. Updates the Mash object with this info. This method is ordinarily not used externally to the object.
 
   Arguments: One mash file
-  Returns:   Reference to a hash
-
-  TODO:      add new subroutine to add by hash
+  Returns:   self
 
 =back
 
 =cut
 
-sub addMashFile{
+sub file{
   my($self,$msh)=@_;
   
-  my %info = %{ $self->{info} };
-
   if(!   $msh){
-    logmsg "WARNING: no file was given to \$self->addMashFile";
+    die "ERROR: no file was given to \$self->file";
     return {};
   }
   if(!-e $msh){
     die "ERROR: could not find file $msh";
   }
 
-  my $mashInfo=from_json(`mash info -d $msh`);
+  my $json=JSON->new;
+  $json->utf8;           # If we only expect characters 0..255. Makes it fast.
+  $json->allow_nonref;   # can convert a non-reference into its corresponding string
+  $json->allow_blessed;  # encode method will not barf when it encounters a blessed reference
+  $json->pretty;         # enables indent, space_before and space_after
 
-  for my $sketch(@{ $$mashInfo{sketches} }){
-    #delete($$sketch{hashes}); logmsg "DEBUG: removing hashes element";
-    $info{$$sketch{name}}=$sketch;
+  my $jsonStr = `mash info -d $msh`;
+  die "ERROR running mash on $msh" if $?;
 
-    my %sketchHash;
-    for my $pos(@{ $$sketch{hashes} }){
-      $$self{hashes}{$pos}++;
-      $sketchHash{$pos}=1;
-    }
-    $info{$$sketch{name}}{hashes}=\%sketchHash;
+  # Need to check for valid utf8 or not
+  eval{ my $strCopy=$jsonStr; decode('utf8', $strCopy, Encode::FB_CROAK) }
+    or die "ERROR: mash info -d yielded non-utf8 characters for this file: $msh";
 
-    # Also take on the general properties of the mash file
-    for my $key(qw(kmer alphabet preserveCase canonical sketchSize hashType hashBits hashSeed)){
-      $info{$$sketch{name}}{$key}=$$mashInfo{$key};
-    }
+  my $mashInfo = $json->decode($jsonStr);
+
+  for my $key(qw(kmer alphabet canonical sketchSize= hashType hashBits hashSeed sketches)){
+    $$self{$key} = $$mashInfo{$key};
   }
-
-  $self->{info}=\%info;
-
-  return \%info;
+  
+  return $self;
 }
 
 =pod
 
 =over
 
-=item $msh->mashDistances()
+=item $msh->dist()
 
-Returns a reference to a hash of mash distances
+Returns a hash describing distances between sketches represented by
+this object and another object. If there are multiple sketches per
+object, then all sketches in this object will be compared against
+all sketches in the other object.
+
+  Arguments: One Mash object
+  Returns:   reference to a hash of hashes. Each value is a number.
+
+Aliases: distance(), mashDist()
 
 =back
 
 =cut
 
-sub mashDistances{
-  my($self)=@_;
-  # Don't recalculate
-  return $self->{distance} if(keys(%{ $self->{distance} }) > 0);
+sub dist{
+  my($self, $other)=@_;
+  my %dist = ();
 
-  my @file=@{$self->{file}};
-  my $numFiles=@file;
-  
-  my %distance;
-  for(my $i=0;$i<$numFiles;$i++){
-    for(my $j=0;$j<$numFiles;$j++){
-      open(my $fh, "mash dist '$file[$i]' '$file[$j]' | ") or die "ERROR: running mash dist on $file[$i] and $file[$j]: $!";
-      while(<$fh>){
-        chomp;
-        my($genome1,$genome2,$dist,$p,$sharedFraction)=split(/\t/,$_);
-        $distance{$genome1}{$genome2}=$dist;
-      }
-      close $fh;
+  my $k = $$self{kmer};
+
+  # TODO check class of $other
+
+  my $numFromSketches = scalar(@{ $self->{sketches} });
+  my $numToSketches   = scalar(@{ $other->{sketches} });
+
+  for(my $i=0; $i<$numFromSketches; $i++){
+    my $fromHashes = $$self{sketches}[$i]{hashes};
+    my $fromName   = $$self{sketches}[$i]{name};
+    for(my $j=0; $j<$numToSketches; $j++){
+      my $toHashes = $$other{sketches}[$j]{hashes};
+      my $toName   = $$other{sketches}[$j]{name};
+
+      my ($common, $total) = raw_mash_distance($fromHashes, $toHashes);
+      my $jaccard = $common/$total;
+      my $mashDist= -1/$k * log(2*$jaccard / (1+$jaccard));
+      $mashDist = sprintf("%0.7f", $mashDist); # rounding to maintain compatibility with exec
+      $dist{$fromName}{$toName} = $mashDist;
+      $dist{$toName}{$fromName} = $mashDist;
     }
   }
-  $self->{distance}=\%distance;
-  return \%distance;
+  return \%dist;
 }
+# Some aliases for dist()
+sub distance{
+  goto &dist;
+}
+sub mashDist{
+  goto &dist;
+}
+
+=pod
+
+=over
+
+=item Mash::raw_mash_distance()
+
+Returns the number of sketches in common and the total number of sketches between two lists.
+The return type is an array of two elements.
+
+  Arguments: A list of integers
+             A list of integers
+  Returns:   (countOfInCommon, totalNumber)
+
+  Example:
+    
+    my $R1 = [1,2,3];
+    my $R2 = [1,2,4];
+    my($common, $total) = Mash::raw_mash_distance($R1,$R2);
+    # $common => 2
+    # $total  => 3
+
+=back
+
+=cut
+
+# https://github.com/onecodex/finch-rs/blob/master/src/distance.rs#L34
+sub raw_mash_distance{
+  my($hashes1, $hashes2) = @_;
+
+  my @sketch1 = sort {$a <=> $b} @$hashes1;
+  my @sketch2 = sort {$a <=> $b} @$hashes2;
+
+  my $i      = 0;
+  my $j      = 0;
+  my $common = 0;
+  my $total  = 0;
+
+  my $sketch_size = @sketch1;
+  while($total < $sketch_size && $i < @sketch1 && $j < @sketch2){
+    my $ltgt = ($sketch1[$i] <=> $sketch2[$j]); # -1 if sketch1 is less than, +1 if sketch1 is greater than
+
+    if($ltgt == -1){
+      $i += 1;
+    } elsif($ltgt == 1){
+      $j += 1;
+    } elsif($ltgt==0) {
+      $i += 1;
+      $j += 1;
+      $common += 1;
+    } else {
+      die "Internal error";
+    }
+
+    $total += 1;
+  }
+
+  if($total < $sketch_size){
+    if($i < @sketch1){
+      $total += @sketch1 - 1;
+    }
+
+    if($j < @sketch2){
+      $total += @sketch2 - 1;
+    }
+
+    if($total > $sketch_size){
+      $total = $sketch_size;
+    }
+  }
+
+  return ($common, $total);
+}
+
 
 ##### Utility methods
 
-sub _reroot_at_midpoint{
-  my($tree)=@_;
-  my $node_with_longest_branch = (sort{
-    my $A=$a->branch_length || 0;
-    my $B=$b->branch_length || 0;
-    $B <=> $A
-  } $tree->get_nodes)[0];
-  my $rootNode=$tree->reroot_at_midpoint($node_with_longest_branch);
-  if(!$rootNode->branch_length || $rootNode->branch_length < 0.01){
-    $rootNode->branch_length(0.01);
-  }
-  return $rootNode;
-}
-
 sub toString{
   my($self)=@_;
-  my $fileArr=$self->{file};
-  my $return="Mash object with " .scalar(@$fileArr)." files:\n\n";
-  
-  for(@$fileArr){
-    $return.="-------${_}------\n";
-    my $info=`mash info '$_'`;
-    chomp($info);
-    $return.=$info;
-    $return.="^^^^^^^${_}^^^^^^\n\n";
+  my $return="Mash object with " .scalar(@{ $self->{sketches} })." file(s):\n";
+  for my $sketch(@{ $self->{sketches} }){
+    $return.=$$sketch{name}."\n";
   }
   
   return $return;
